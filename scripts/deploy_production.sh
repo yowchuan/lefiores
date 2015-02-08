9e20e5ef7cd0528a1fc5f7abff847f4875d9b573
emilenriquez <emil.nrqz@gmail.com>
2015-02-08 21:08:44 +0800

    circleci automatic deploy update configuration

---
+++ scripts/deploy_production.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+bundle exec cap production deploy
+bundle exec cap production lefiores:install_bundle
+
+bundle exec cap production lefiores:compile_assets
+bundle exec cap production lefiores:restart
+
