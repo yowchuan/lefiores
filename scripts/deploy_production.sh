168c30225b295b691cd148447bb8f07e790e3748
emilenriquez <emil.nrqz@gmail.com>
2015-02-09 01:02:03 +0800

    edited deployment config

--- scripts/deploy_production.sh
+++ scripts/deploy_production.sh
@@ -4,5 +4,6 @@ bundle exec cap production deploy
 bundle exec cap production lefiores:install_bundle
 
 bundle exec cap production lefiores:compile_assets
-bundle exec cap production lefiores:restart
+bundle exec cap production lefiores:stop
+bundle exec cap production lefiores:start
 
